9a85c4914d895969fc6de088441ac4aaadc33464
anderzzz <ohrn.anders@gmail.com>
2019-10-18 10:29:12 -0700

    Basic files, no meaningful content yet

---
+++ buyer.py
@@ -0,0 +1,7 @@
+from fjarrsyn import Agent
+
+class Buyer(Agent):
+
+    def __init__(self, name):
+
+        super().__init__(name)
---
+++ tmp.py
@@ -0,0 +1,6 @@
+from buyer import Buyer
+
+by_1 = Buyer('Buyer 1')
+
+print (by_1)
+print (by_1.name)
